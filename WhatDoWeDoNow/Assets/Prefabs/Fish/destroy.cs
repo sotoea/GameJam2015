503811fa66c2b4a349c969def035f3b7bb37f4d1
Eduardo Soto <easm93@gmail.com>
2015-01-25 02:43:50 -0500

    Copy?

---
+++ WhatDoWeDoNow/Assets/Prefabs/Fish/destroy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroy : MonoBehaviour {
+	public GameObject fishPlane;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y<-20)
+			Destroy(this.gameObject);
+	}
+
+	void OnTriggerEnter(Collider collision) {
+		if(collision.name == "fish_02"){
+		rigidbody.velocity = transform.TransformDirection(-rigidbody.velocity.x*0.5f,-rigidbody.velocity.y*0.2f,-rigidbody.velocity.z*0.5f);
+		collision.animation.Play("Death");
+		Instantiate(fishPlane,collision.transform.position-(new Vector3(1.85f,-0.5f,0f)), Quaternion.Euler(0,0,0));
+			fishPlane.transform.parent = collision.transform;
+			//collision.SendMessage("changeMove");
+		}
+		
+	}
+}
---
+++ WhatDoWeDoNow/Assets/Scripts/sortingOrder.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+public class sortingOrder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Set the sorting layer of the particle system.
+		particleSystem.renderer.sortingLayerName = "background";
+		particleSystem.renderer.sortingOrder = 0;
+	}		
+}
